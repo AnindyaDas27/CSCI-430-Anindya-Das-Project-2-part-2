d4ffb509e5e0cacdb9f7db974b80f3a7cd314dac
AnindyaDas27 <119897110+AnindyaDas27@users.noreply.github.com>
2025-11-15 11:30:44 -0600

    Update ClerkPanel.java

--- src/ui/ClerkPanel.java
+++ src/ui/ClerkPanel.java
@@ -1,1 +1,26 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ClerkPanel extends UIState {
+    public ClerkPanel(UIContext ctx) {
+        super(ctx);
+        setLayout(new GridLayout(0,1,5,5));
+
+        add(new JLabel("Clerk Menu", SwingConstants.CENTER));
+
+        JButton addClient = new JButton("Add Client");
+        JButton showClients = new JButton("Show Clients");
+        JButton recordPayment = new JButton("Record Payment");
+        JButton logout = new JButton("Logout");
+
+        logout.addActionListener(e -> goTo("LOGIN"));
+
+        add(addClient);
+        add(showClients);
+        add(recordPayment);
+        add(logout);
+    }
+}
 
